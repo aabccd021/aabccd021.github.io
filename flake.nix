20a687982ab16a5c9763b72ce0dbba6a5231afcc
aabccd021 <aabccd021@gmail.com>
2022-10-13 07:50:08 +0700

    refactor: better flake

--- flake.nix
+++ flake.nix
@@ -3,12 +3,15 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShells.default = import ./shell.nix { inherit pkgs; };
-        }
-      );
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (
+    system: with nixpkgs.legacyPackages.${system};{
+      devShells.default = mkShell {
+        buildInputs = [
+          nodejs-18_x
+          nodePackages.pnpm
+          nodePackages.eslint_d
+        ];
+      };
+    }
+  );
 }
