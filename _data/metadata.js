7f9aab05f7efe5d19213adaf7d1898199a791028
aabccd021 <aabccd021@gmail.com>
2023-02-02 16:26:24 +0700

    fix: description

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	"title": "aabccd021 blog",
 	"url": "https://aabccd021.github.io",
 	"language": "en",
-	"description": "Blog of aabccd021 (Muhamad Abdurahman)",
+	"description": "Blog of aabccd021 (Muhamad Abdurahman). Mostly about software engineering",
 	"author": {
 		"name": "Muhamad Abdurahman",
 		"email": "aabccd021@gmail.com",
