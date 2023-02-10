46eee58981d93b0470a6d66fdd3679d85abde39e
aabccd021 <aabccd021@gmail.com>
2023-02-10 10:04:26 +0700

    fix: should fail

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
         lychee
       ];
       shellHook = ''
-        for npm_dir in $(git ls-files | grep pnpm-lock.yaml); do pnpm install --dir $(dirname "$npm_dir"); done || exit 1
+        pnpm install --recursive
       '';
     };
   };
