5097bbaf7af73f85d1d6bcb080c61f7e1eeab2c9
aabccd021 <aabccd021@gmail.com>
2023-02-10 10:06:01 +0700

    Revert "fix: should fail"
    This reverts commit 46eee58981d93b0470a6d66fdd3679d85abde39e.

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
         lychee
       ];
       shellHook = ''
-        pnpm install --recursive
+        for npm_dir in $(git ls-files | grep pnpm-lock.yaml); do pnpm install --dir $(dirname "$npm_dir"); done || exit 1
       '';
     };
   };
