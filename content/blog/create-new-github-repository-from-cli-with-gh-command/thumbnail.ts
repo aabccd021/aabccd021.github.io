8413be3164579d9eb8c8971d2afb689dd09d4a36
aabccd021 <aabccd021@gmail.com>
2023-02-14 15:41:35 +0700

    fix: no jsx

--- content/blog/create-new-github-repository-from-cli-with-gh-command/thumbnail.ts
+++ content/blog/create-new-github-repository-from-cli-with-gh-command/thumbnail.ts
@@ -1,17 +1,24 @@
 import { generate } from '../../../satori/generate';
 
-const element = (
-  <div
-    style={{
+const element: React.ReactNode = {
+  key: '',
+  type: 'div',
+  props: {
+    style: {
       color: '#d4be98',
       backgroundColor: '#282828',
       height: '100%',
       width: '100%',
       display: 'flex',
-    }}
-  >
-    <pre style={{ margin: 'auto', fontSize: 140 }}>❯ gh repo create</pre>
-  </div>
-);
+    },
+    children: {
+      type: 'pre',
+      props: {
+        tw: 'm-auto text-[#d4be98] text-[140px]',
+        children: '❯ gh repo create',
+      },
+    },
+  },
+};
 
 export const main = () => generate({ element, output: `${__dirname}/thumbnail.svg` });
