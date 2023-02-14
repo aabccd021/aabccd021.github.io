11ec87dd6545c974ad93b3b54401cb2cd8bb9a8d
aabccd021 <aabccd021@gmail.com>
2023-02-14 10:08:18 +0700

    fix: satori nodemon

--- aab.tsx
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line import/no-nodejs-modules
-
-import React from 'react';
-
-export const img = <div style={{ color: 'red' }}>hello, aaa</div>;
---
+++ satori/generate.ts
@@ -0,0 +1,30 @@
+// eslint-disable-next-line import/no-nodejs-modules
+import * as fs from 'node:fs/promises';
+
+import satori from 'satori';
+
+export const generate = async ({
+  element,
+  output,
+  width,
+  height,
+}: {
+  readonly element: React.ReactNode;
+  readonly output: string;
+  readonly width?: number;
+  readonly height?: number;
+}) => {
+  const result = await satori(element, {
+    width: width ?? 1200,
+    height: height ?? 630,
+    fonts: [
+      {
+        name: 'Inter',
+        data: await fs.readFile(`${__dirname}/inter-latin-ext-700-normal.woff`),
+        weight: 400,
+        style: 'normal',
+      },
+    ],
+  });
+  return fs.writeFile(output, result, { encoding: 'utf8' });
+};
