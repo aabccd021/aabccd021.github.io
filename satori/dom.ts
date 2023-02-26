2bf24eeeb7bb9fb57686ac4301f42cace5daa4e7
aabccd021 <aabccd021@gmail.com>
2023-02-26 15:57:47 +0700

    fix: html

--- satori/dom.ts
+++ satori/dom.ts
@@ -1,33 +1,53 @@
-import type { ClassAttributes, DOMAttributes, HTMLAttributes, ReactDOM } from 'react';
+import type * as React from 'react';
 
-export type Tag = keyof ReactDOM;
-
-export type ReactSpecificAttributes =
-  | 'defaultChecked'
-  | 'defaultValue'
-  | 'suppressContentEditableWarning'
-  | 'suppressHydrationWarning';
-
-export type ComponentSpecificEventHandler = 'onCancel' | 'onClose' | 'onToggle';
-
-export type OmitReactAttribues<T> = T extends HTMLAttributes<infer K>
+type OmitReactAttribues<T> = T extends React.HTMLAttributes<infer K>
   ? Omit<
       T,
-      | ComponentSpecificEventHandler
-      | ReactSpecificAttributes
-      | keyof ClassAttributes<K>
-      | keyof DOMAttributes<K>
+      | keyof React.ClassAttributes<K>
+      | keyof React.DOMAttributes<K>
+      | 'defaultChecked'
+      | 'defaultValue'
+      | 'onCancel'
+      | 'onClose'
+      | 'onToggle'
+      | 'suppressContentEditableWarning'
+      | 'suppressHydrationWarning'
     >
   : never;
 
-export type Attributes<T extends Tag> = OmitReactAttribues<NonNullable<Parameters<ReactDOM[T]>[0]>>;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type NonNullableFirstParam<T extends (...args: any) => any> = NonNullable<Parameters<T>[0]>;
 
-export const a: Attributes<'a'> = {
+export type HTMLTag = keyof React.ReactHTML;
+
+export type SVGTag = keyof React.ReactSVG;
+
+export type HTMLAttributes<T extends HTMLTag> = OmitReactAttribues<
+  NonNullableFirstParam<React.ReactHTML[T]>
+>;
+
+export type SVGAttributes<T extends SVGTag> = OmitReactAttribues<
+  NonNullableFirstParam<React.ReactSVG[T]>
+>;
+
+export const a: HTMLAttributes<'a'> = {
   'aria-atomic': 'true',
 };
 
-export const dialog: Attributes<'dialog'> = {};
+export const dialog: HTMLAttributes<'dialog'> = {};
 
-export const anmiate: Attributes<'clipPath'> = {
+export const anmiate: SVGAttributes<'svg'> = {
   accentHeight: 10,
+};
+
+export type SVGElement<T extends SVGTag> = {
+  readonly tag: T;
+  readonly attribues: SVGAttributes<T>;
+  readonly children: readonly SVGElement<T>[];
+};
+
+export type HTMLElement<T extends HTMLTag> = {
+  readonly tag: T;
+  readonly attribues: HTMLAttributes<T>;
+  readonly children: HTMLElement<HTMLTag> | readonly SVGElement<'svg'>[];
 };
